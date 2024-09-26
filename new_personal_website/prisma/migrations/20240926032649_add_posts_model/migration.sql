6747d544256652f47da93404042c41f18a303246
Yao Guan <yaozongguan@gmail.com>
2024-09-25 23:35:34 -0400

    update to schema and setting up adding user into database after account creation

---
+++ new_personal_website/prisma/migrations/20240926032649_add_posts_model/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+ADD COLUMN     "firstName" TEXT,
+ADD COLUMN     "lastName" TEXT,
+ADD COLUMN     "password" TEXT,
+ADD COLUMN     "supabaseId" TEXT;
